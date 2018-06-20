2bf3584f5cae0a859f65e1e71116e1217be76fca
Amazonka1 <39888397+Amazonka1@users.noreply.github.com>
2018-06-21 01:41:59 +0300

    top-button

--- coffee/js/common.js
+++ coffee/js/common.js
@@ -1,8 +1,8 @@
 $(document).ready(function(){
-	$(".top-button").click(function(){
-		var elementClick = $(this).attr("href");
-		var destination = $(elementClick).offset().top;
-        $('html').animate({ scrollTop: destination }, 500);
-        return false;
+	$(".top-button").on("click touchstart touchend", function(e){
+		//var elementClick = $(this).attr("href");
+		//e.preventDefault();
+		//var destination = $(elementClick).offset().top;
+    $('html').animate({ scrollTop: 0 }, 500);
 	});
 });
