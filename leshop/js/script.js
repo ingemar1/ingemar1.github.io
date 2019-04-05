390d17803a95ea01ff3bb7631345643c589680cd
Amazonka1 <39888397+Amazonka1@users.noreply.github.com>
2019-04-05 15:09:40 +0300

    leshop and realty

---
+++ leshop/js/script.js
@@ -0,0 +1,183 @@
+﻿$(document).ready(function(){
+    $("#fancy_mail").fancybox();
+    $(".fancy_view_btn").fancybox();
+    $("#fancy_call").fancybox();
+	$("#fancy_password").fancybox();
+	$("#fancy_password_answer").fancybox();
+	// $(".fancy_basket_btn").fancybox();
+	$(".fancy_basket_btn").submit(function(){
+		$.fancybox({
+			href: '#fancybox_basket', 
+			modal: true
+		});
+		 return true;
+	});
+	tabs(".my_account", ".personalCabinet-tabs li", "active", 1, "tabLk_");
+	tabs(".my_account-orders", ".my_account-menu li", "active", 0, "tabs_");
+	tabs(".tab_content", "#product-tabs.tabs li", "active", 1, "tabs_");
+	tabs(".basket_table", ".basket_tabs li", "active", 0, "tabs_");
+	function tabs(tabs_content, tabs_menu, menu_tab_active, show_tab, id_tabs_content){
+		$(tabs_content).hide();
+		$(tabs_content).eq(show_tab).show();
+		$(tabs_menu).eq(show_tab).addClass(menu_tab_active);
+		$(tabs_menu).on("click", function(){
+			$(tabs_menu).removeClass(menu_tab_active);
+			$(this).addClass(menu_tab_active);
+			$(tabs_content).hide();
+			$(tabs_content + "#" + id_tabs_content + $(this).attr("id")).show();
+		});
+	}
+	
+	if($(".basket_table").children().is(".basket_empty")){
+		$(".basket_btns.print").hide();
+	}
+	if($(".basket_table").children().is(".basket_empty")){
+		$(".basket_btns.remove").hide();
+	}
+	$(".basket_tabs li").on("click", function(){
+		if($(".basket_tabs li").eq(1).is(".active")){
+			$(".basket_btns.print").hide();
+		}else if($(".basket_table").children().is(".basket_empty")){
+			$(".basket_btns.print").hide();
+		}else{
+			$(".basket_btns.print").show();
+		}
+		
+		if($(".basket_table").children().is(".basket_empty") && $(".basket_tabs li").eq(0).is(".active")){
+			$(".basket_btns.remove").hide();
+		}else{
+			$(".basket_btns.remove").show();
+		}
+	});
+	
+	$(".more").on("click", function(){
+		if($(this).parent().nextAll(".my_account-order").css("display") == "none"){
+			$(this).children().addClass("icon-triangle_top").next().text("свернуть");
+			$(this).parent().nextAll(".my_account-order").show();
+		}else{
+			$(this).children().removeClass("icon-triangle_top").next().text("подробнее");
+			$(this).parent().nextAll(".my_account-order").hide();
+		}
+	});
+	
+	$(".catalog-menu li").on("click", function(){
+		if($(this).children().next().css("display") == "none"){
+			$(this).children().next().show();
+		}else{
+			$(this).children().next().hide();
+		}
+		return false;
+	});
+	
+	$(".menu li").hover(function(){
+		$(".under_menu").hide();
+		var idMenu = $(this).attr("id");
+		$("#sub_" + idMenu).show();
+		$(".top3").on({
+			mouseleave: function(){
+				$("#sub_" + idMenu).hide();
+			}
+		});
+	});
+	
+	$("#give_feedback").on("click", function(){
+		$(this).hide();
+		$("#reviews").hide();
+		$("#review-form").show();
+		return false;
+	});
+	
+	$('.rating_star.large.hover span').on({
+		mouseenter: function(){	   
+			$(this).prevAll().addClass("selected");
+			$(this).addClass("selected");
+		},
+		mouseleave: function(){
+			$(".rating_star.large.hover span").removeClass("selected");
+			if($(".rating_star.large.hover span").is(".click")){
+				$(".click").prevAll().addClass("selected");
+				$(".click").addClass("selected");
+			}
+		},
+		click: function(){
+			$(".rating_star.large.hover span").removeClass("selected");
+			$(this).prevAll().addClass("selected");
+			$(this).addClass("selected");
+			$(".rating_star.large.hover span").removeClass("click");
+			$(this).addClass("click");
+		}
+	});
+	
+	$('input.quantity-entry').each(function() {
+		var $input = $(this);
+		$input.prev().click(function(e) {
+			e.preventDefault();
+			$input.val(Math.max(parseInt($input.val())-1, 1));
+		});
+		$input.next().click(function(e) {
+			e.preventDefault();
+			$input.val(parseInt($input.val())+1);
+		});
+	});
+	
+	/*$("#slider-ui1").slider({
+		range: true,
+		min: 1,
+		max: 700000,
+		values: [4000, 80000],
+		slide: function(event, ui){
+			$("#filter-value_min").val(ui.values[0].toLocaleString());
+			$("#filter-value_max").val(ui.values[1].toLocaleString());
+		}
+	});
+	$("#filter-value_min").val($("#slider-ui1").slider("values", 0).toLocaleString());
+	$("#filter-value_max").val($("#slider-ui1").slider("values", 1).toLocaleString());*/
+
+
+})
+// required — поле обязательное для заполнения (true или false)
+// remote — указывается файл для проверки поля (например: check.php)
+// email — проверяет корректность e-mail адреса (true или false)
+// url — проверяет корректность url адреса (true или false)
+// date — проверка корректности даты (true или false)
+// dateISO — проверка корректности даты ISO (true или false)
+// number — проверка на число (true или false)
+// digits — только цифры (true или false)
+// creditcard — корректность номера кредитной карты (true или false)
+// equalTo — равное чему-то (например другому полю equalTo:»#pswd»)
+// accept — проверка на правильное расширение (accept: «xls|csv»)
+// maxlength — максимальное кол-во символов (число)
+// minlength — минимальное кол-во символов (число)
+// rangelength — кол-во символов от скольки и до скольки (rangelength: [2, 6])
+// range — число должно быть в диапазоне от и до (range: [13, 23])
+// max — максимальное значение числа (22)
+// min — минимальное значение числа (11)
+//
+/*$(document).ready(function(){
+	$("#loginform").validate({
+		rules:{
+			login:{
+				required: true,
+				minlength: 4,
+				maxlength: 16,
+			},
+			pswd:{
+				required: true,
+				minlength: 6,
+				maxlength: 16,
+			},
+		},
+		messages:{
+			login:{
+				required: "Это поле обязательно для заполнения",
+				minlength: "Логин должен быть минимум 4 символа",
+				maxlength: "Максимальное число символо - 16",
+			},
+			pswd:{
+				required: "Это поле обязательно для заполнения",
+				minlength: "Пароль должен быть минимум 6 символа",
+				maxlength: "Пароль должен быть максимум 16 символов",
+			},
+		}
+	});
+});*/
---
+++ site-realty/js/script.js
@@ -0,0 +1,133 @@
+﻿$(document).ready(function(){
+	function map_styles(){
+		var map_style = [{"featureType":"all","elementType":"labels.text.fill","stylers":[{"saturation":36},{"color":"#333333"},{"lightness":40}]},{"featureType":"all","elementType":"labels.text.stroke","stylers":[{"visibility":"on"},{"color":"#ffffff"},{"lightness":16}]},{"featureType":"all","elementType":"labels.icon","stylers":[{"visibility":"off"}]},{"featureType":"administrative","elementType":"all","stylers":[{"visibility":"off"}]},{"featureType":"administrative","elementType":"geometry.fill","stylers":[{"color":"#fefefe"},{"lightness":20}]},{"featureType":"administrative","elementType":"geometry.stroke","stylers":[{"color":"#fefefe"},{"lightness":17},{"weight":1.2}]},{"featureType":"administrative.land_parcel","elementType":"all","stylers":[{"visibility":"off"}]},{"featureType":"landscape","elementType":"geometry","stylers":[{"lightness":20},{"color":"#ececec"}]},{"featureType":"landscape.man_made","elementType":"all","stylers":[{"visibility":"on"},{"color":"#f0f0ef"}]},{"featureType":"landscape.man_made","elementType":"geometry.fill","stylers":[{"visibility":"on"},{"color":"#f0f0ef"}]},{"featureType":"landscape.man_made","elementType":"geometry.stroke","stylers":[{"visibility":"on"},{"color":"#d4d4d4"}]},{"featureType":"landscape.natural","elementType":"all","stylers":[{"visibility":"on"},{"color":"#ececec"}]},{"featureType":"poi","elementType":"all","stylers":[{"visibility":"on"}]},{"featureType":"poi","elementType":"geometry","stylers":[{"lightness":21},{"visibility":"off"}]},{"featureType":"poi","elementType":"geometry.fill","stylers":[{"visibility":"on"},{"color":"#d4d4d4"}]},{"featureType":"poi","elementType":"labels.text.fill","stylers":[{"color":"#303030"}]},{"featureType":"poi","elementType":"labels.icon","stylers":[{"saturation":"-100"}]},{"featureType":"poi.attraction","elementType":"all","stylers":[{"visibility":"off"}]},{"featureType":"poi.business","elementType":"all","stylers":[{"visibility":"off"}]},{"featureType":"poi.government","elementType":"all","stylers":[{"visibility":"off"}]},{"featureType":"poi.medical","elementType":"all","stylers":[{"visibility":"off"}]},{"featureType":"poi.park","elementType":"all","stylers":[{"visibility":"on"}]},{"featureType":"poi.park","elementType":"geometry","stylers":[{"color":"#dedede"},{"lightness":21}]},{"featureType":"poi.place_of_worship","elementType":"all","stylers":[{"visibility":"on"}]},{"featureType":"poi.school","elementType":"all","stylers":[{"visibility":"on"}]},{"featureType":"poi.school","elementType":"geometry.stroke","stylers":[{"lightness":"-61"},{"gamma":"0.00"},{"visibility":"off"}]},{"featureType":"poi.sports_complex","elementType":"all","stylers":[{"visibility":"on"}]},{"featureType":"road.highway","elementType":"geometry.fill","stylers":[{"color":"#ffffff"},{"lightness":17}]},{"featureType":"road.highway","elementType":"geometry.stroke","stylers":[{"color":"#ffffff"},{"lightness":29},{"weight":0.2}]},{"featureType":"road.arterial","elementType":"geometry","stylers":[{"color":"#ffffff"},{"lightness":18}]},{"featureType":"road.local","elementType":"geometry","stylers":[{"color":"#ffffff"},{"lightness":16}]},{"featureType":"transit","elementType":"geometry","stylers":[{"color":"#f2f2f2"},{"lightness":19}]},{"featureType":"transit.station.bus","elementType":"labels.icon","stylers":[{"visibility":"on"},{"saturation":"-100"}]},{"featureType":"water","elementType":"geometry","stylers":[{"color":"#dadada"},{"lightness":17}]}];
+		return map_style;
+	}
+	// When the window has finished loading create our google map below
+	google.maps.event.addDomListener(window, 'load', init);
+
+	function init() {
+		// Basic options for a simple Google Map
+		// For more options see: https://developers.google.com/maps/documentation/javascript/reference#MapOptions
+		var mapOptions = {
+			// How zoomed in you want the map to start at (always required)
+			zoom: 16,
+
+			// The latitude and longitude to center the map (always required)
+			center: new google.maps.LatLng(46.282489, 7.532631),
+
+			// How you would like to style the map. 
+			// This is where you would paste any style found on Snazzy Maps.
+			styles:map_styles()
+		};
+
+		// Get the HTML DOM element that will contain your map 
+		// We are using a div with id="map" seen below in the <body>
+		var mapElement = document.getElementById('map');
+
+		// Create the Google Map using our element and options defined above
+		var map = new google.maps.Map(mapElement, mapOptions);
+
+		// Let's also add a marker while we're at it
+		var marker = new google.maps.Marker({
+			position: new google.maps.LatLng(46.282489, 7.532631),
+			map: map,
+			title: 'Snazzy!'
+		});
+	}
+		function initialize() {
+          var mapOptions = {
+                  zoom: 16,
+                  center: new google.maps.LatLng(46.282489, 7.532631),
+				  styles: map_styles()
+              },
+              map = new google.maps.Map(document.getElementById('popup_map'),
+              mapOptions),
+              marker = new google.maps.Marker({
+                  map: map,
+                  position: map.getCenter()
+              });
+			
+               $('#fancybutton_map').click(function () {
+					  $.fancybox(map.getDiv(),
+
+					  {
+						  width: 600,
+						  height: 400,
+						  autoSize: false,
+						  afterShow: function (a, z) {
+							  google.maps.event.trigger(map, 'resize');
+							  map.setCenter(this.content.data('center'));
+						  },
+
+						  beforeLoad: function (a) {
+							  this.content.data({
+								  parent: this.content.parent(),
+								  center: map.getCenter()
+							  })
+						  }
+					  });
+
+			});
+
+      }
+
+      google.maps.event.addDomListener(window, 'load', initialize);
+	
+	$("#slider-ui1").slider({
+		range: true,
+		min: 150,
+		max: 20000000,
+		values: [150, 20000000],
+		slide: function(event, ui){
+			$("#filter-value_min").val("€ " + ui.values[0].toLocaleString());
+			$("#filter-value_max").val("€ " + ui.values[1].toLocaleString());
+		}
+	});
+	$("#filter-value_min").val("€ " + $("#slider-ui1").slider("values", 0).toLocaleString());
+	$("#filter-value_max").val("€ " + $("#slider-ui1").slider("values", 1).toLocaleString());
+	
+	$("#slider-ui2").slider({
+		range: true,
+		min: 1500,
+		max: 9000,
+		values: [1500, 9000],
+		slide: function(event, ui){
+			$("#filter-value_min2").val(ui.values[0].toLocaleString() + " Sq. Ft.");
+			$("#filter-value_max2").val(ui.values[1].toLocaleString() + " Sq. Ft.");
+		}
+	});
+	$("#filter-value_min2").val($("#slider-ui2").slider("values", 0).toLocaleString() + " Sq. Ft.");
+	$("#filter-value_max2").val($("#slider-ui2").slider("values", 1).toLocaleString() + " Sq. Ft.");
+	
+	$("#slider-ui3").slider({
+		range: true,
+		min: 4,
+		max: 12,
+		values: [4, 12],
+		slide: function(event, ui){
+			$("#filter-value_min3").val(ui.values[0] + " rooms");
+			$("#filter-value_max3").val(ui.values[1] + " rooms");
+		}
+	});
+	$("#filter-value_min3").val($("#slider-ui3").slider("values", 0) + " rooms");
+	$("#filter-value_max3").val($("#slider-ui3").slider("values", 1) + " rooms");
+	
+	 //$( "#selectable1" ).selectable();
+	 //$( "#selectable2" ).selectable();  
+      //$(".chosen-select-width").chosen();
+	  $(".chosen-select-width").chosenImage({
+			disable_search_threshold: 10
+		});
+	function heightDetermination(select){
+		var highColumn = 0;
+		$(select).each(function(){
+			var currentHeight = $(this).height();
+			if(currentHeight > highColumn){
+				highColumn = currentHeight;
+			}
+		});
+		$(select).height(highColumn);
+	}
+	heightDetermination(".card_product-similar_products-description")
+});
